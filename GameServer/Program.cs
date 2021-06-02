6aff88764674adb8ab853de14ee21cb0f7d339ad
px <px@DESKTOP-04CUGA2>
2021-06-02 16:16:23 +0200

    Projektdateien hinzufügen.

---
+++ GameServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.Title = "Game Server";
+
+            Server.Start(50, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
